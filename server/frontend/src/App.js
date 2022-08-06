8a33215dba5908b864a5110b7e32c014e4ec6396
ritsuri <warit.srisuriyarungrueng@gmail.com>
2022-08-06 05:16:53 +0000

    update frontend

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -7,6 +7,18 @@ import ContactUs from './contactus.js';
 Copy the Web Chat code from Watson Assitant and paste it here
 */
 
+window.watsonAssistantChatOptions = {
+    integrationID: "c9ce80d0-cdc7-4400-bba0-9a77bbcc7cc2", // The ID of this integration.
+    region: "au-syd", // The region your integration is hosted in.
+    serviceInstanceID: "5052c3de-fa81-466d-a13d-8aef67d13e98", // The ID of your service instance.
+    onLoad: function(instance) { instance.render(); }
+  };
+setTimeout(function(){
+  const t=document.createElement('script');
+  t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js"
+  document.head.appendChild(t);
+});
+
 class App extends React.Component {
 
   state = {
@@ -30,10 +42,15 @@ class App extends React.Component {
               <li class="nav-item active">
                 <a class="nav-link" href="#" onClick={this.setPageHome}>Home <span class="sr-only">(current)</span></a>
               </li>
-                /*
-                Add more tabs and action items for the tabs to give the dealership website a real feel. 
-                */
-        
+              <li class="nav-item">
+               <a class="nav-link" href="#" onClick={this.setPageHome}>About us <span class="sr-only">(current)</span></a>
+              </li> 
+              <li class="nav-item">
+               <a class="nav-link" href="#" onClick={this.setPageHome}>Dealer Network <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+               <a class="nav-link" href="#" onClick={this.setPageHome}>Location <span class="sr-only">(current)</span></a>
+              </li>
               <li class="nav-item">
                 <a class="nav-link" href="#" onClick={this.setPageContactUs}>Contact Us</a>
               </li>
